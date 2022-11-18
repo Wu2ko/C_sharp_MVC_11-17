1e2d1377f64209576be0e4f178944a9b022514de
Wu2ko <98595248+Wu2ko@users.noreply.github.com>
2022-11-18 10:51:10 +0200

    Add files via upload

---
+++ MVC_11-17/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MVC_11_17.Models;
+
+namespace MVC_11_17.Controllers
+{
+    public class AccountController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AccountView()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            return RedirectToAction("Index");
+        }
+
+        
+
+    }
+}
--- MVC_11-17/Controllers/HomeController.cs
+++ MVC_11-17/Controllers/HomeController.cs
@@ -10,47 +10,52 @@ namespace MVC_11_17.Controllers
 {
     public class HomeController : Controller
     {
-
-        public IActionResult Index(InputViewModel model)
-        {
-            // string mystring = "sveiki";
-            //  string mystring2 = "sveiki22222";
-            var inputs = new InputViewModel();
-            
-
-
-            var manoObjektas = new ManoTest();
-
-            manoObjektas.Query = model.name;
-            manoObjektas.Skaicius = model.skaic;
-            
-            int sudetis = model.q + model.skaic;
-
-            //string naujasZodis;
-            /*
-            ViewBag.queryStringas = q;
-            ViewBag.queryIntas = skaic;
-
-
-            ViewBag.manoStringas = mystring;
-            ViewData["testtext"] = mystring;
-            TempData["tes2"] = mystring;
-            */
-            if (ModelState.IsValid)
-            {
-                return View("Index", manoObjektas);
-            }
-            else
-            return View("Error");
-
-            
-            
-        }
-        public IActionResult Verslas(string q)
+        public IActionResult Index(InputViewModel name)
         {
             return View();
 
         }
+        public IActionResult Privacy()
+            {
+            return View();
     }
+
+    //  public IActionResult Index(InputViewModel model)
+    //  {
+    /*// string mystring = "sveiki";
+    //  string mystring2 = "sveiki22222";
+    var inputs = new InputViewModel();
+
+
+
+    var manoObjektas = new ManoTest();
+
+    manoObjektas.Query = model.name;
+    manoObjektas.Skaicius = model.skaic;
+
+    int sudetis = model.q + model.skaic;
+
+    //string naujasZodis;
+    /*
+    ViewBag.queryStringas = q;
+    ViewBag.queryIntas = skaic;
+
+
+    ViewBag.manoStringas = mystring;
+    ViewData["testtext"] = mystring;
+    TempData["tes2"] = mystring;
+    *//*
+    if (ModelState.IsValid)
+    {
+        return View("Index", manoObjektas);
+    }
+    else
+    return View("Error");
+
+    */
+
+    //  }
+
+}
 }
 // ?q=reiksme
